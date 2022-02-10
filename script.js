c2a9e459a276ede7f23da17d8e40dc44ff6e686b
unknown <milesan_calin@yahoo.com>
2022-02-10 16:52:18 +0200

    Good designe

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const APIkey = "5c22c923e1496b8c08f6409b48327fd6";
 const searchBtn = document.querySelector(".button");
 const searchBar = document.querySelector(".search-bar");
-const info = document.querySelector(".info");
+const secCard = document.querySelector(".section-card");
 const city = document.querySelector(".city");
 
 const getWeather = async function (location) {
@@ -10,9 +10,8 @@ const getWeather = async function (location) {
       `https://api.openweathermap.org/data/2.5/find?q=${location}&units=metric&appid=${APIkey}`
     );
     const data = await res.json();
-    const {lat: lat, lon: lon} = data.list[0].coord;
-    console.log(lat, lon)
-    const part = 'current';
+    const { lat: lat, lon: lon } = data.list[0].coord;
+    const part = "current";
 
     const resForcast = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}`
@@ -30,34 +29,35 @@ const renderWeather = function (data, dataForcast) {
   const date = new Date();
   console.log(weatherData);
   const markup = `
+  <div class="card">
+  <div class="info">
     <div class="temp-info">
-        <h1 class="city">${weatherData.name}</h1>
-        <span><p class="date">${date.getDate()}.${
-    date.getMonth() + 1
-  }.${date.getFullYear()}</p></span>
-        <h2 class="temp">${Math.round(weatherData.main.temp)}°C</h2>
-        <p class="weather">${weatherData.weather[0].main}</p>
-        <p class="min-max">min: ${Math.round(
-          weatherData.main.temp_min
-        )}°C/max: ${Math.round(weatherData.main.temp_max)}°C</p>
+      <h1 class="city">${weatherData.name}</h1>
+      <span><p class="date">${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}</p></span>
+      <h2 class="temp">${Math.round(weatherData.main.temp)}°C</h2>
+      <p class="weather">${weatherData.weather[0].main}</p>
+      <p class="min-max">min: ${Math.round(weatherData.main.temp_min)}°C/max: ${Math.round(weatherData.main.temp_max)}°C</p>
     </div>
     <div class="other-info">
-        <div class="humidity small-info">
-            <p class="small-partange">${weatherData.main.humidity}%</p>
-            <span class="small-text">Humidity</span>
-        </div>
-        <div class="wind small-info">
-            <p class="small-partange">${weatherData.wind.speed}m/s</p>
-            <span class="small-text">Wind speed</span>
-        </div>
-        <div class="cloudiness small-info">
-            <p class="small-partange">${weatherData.clouds.all}%</p>
-            <span class="small-text">Cloudiness</span>
-        </div>
+      <div class="humidity small-info">
+        <p class="small-partange">${weatherData.main.humidity}%</p>
+        <span class="small-text">Humidity</span>
+      </div>
+      <div class="wind small-info">
+        <p class="small-partange">${weatherData.wind.speed}m/s</p>
+        <span class="small-text">Wind speed</span>
+      </div>
+      <div class="cloudiness small-info">
+        <p class="small-partange">${weatherData.clouds.all}%</p>
+        <span class="small-text">Cloudiness</span>
+      </div>
     </div>
+  </div>
+</div>
     `;
-  info.insertAdjacentHTML("afterend", markup);
-  console.log(dataForcast)
+    secCard.innerHTML = '';
+    secCard.insertAdjacentHTML("afterbegin", markup);
+  console.log(dataForcast);
 };
 
 searchBtn.addEventListener("click", function () {
